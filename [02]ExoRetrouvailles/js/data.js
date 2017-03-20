2f431939894e63251701a9cce645c6b3c10af37f
hananekharraf <hananekharraf@lepoles.com>
2017-03-20 17:04:45 +0100

    ajout exo javascript

---
+++ [02]ExoRetrouvailles/js/data.js
@@ -0,0 +1,12 @@
+var data = {
+  'perso_0' : {
+    'name' : 'Indila',
+    'picture' : 'img/Indila.jpg',
+    'texte' : [' Ça fait dix années que j’avance en musique ','Ça fait dix années que j’avance en musique']
+    },
+  'perso_1' : {
+    'name' : 'journaliste',
+    'picture' : 'img/journaliste.jpg',
+    'texte' : ['Quel est ton parcours musical ?, ','J\'aime les pâttes moi aussi','Pff nimporte quoi!']
+  }
+}
